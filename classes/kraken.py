04e33003177a29bf1713a94cd64dfc160745edb4
Xander Dyer <leakyf@protonmail.com>
2023-02-05 13:27:19 -0500

    working get account balance call

--- classes/kraken.py
+++ classes/kraken.py
@@ -1,33 +1,34 @@
 import os
-import time
 import requests
 import urllib.parse
 import hashlib
 import hmac
 import base64
+from dotenv import load_dotenv
 
 class KrakenClient: 
     def __init__(self) -> None:
         # Read Kraken API key and secret stored in environment variables
+        load_dotenv()
         self._api__url = "https://api.kraken.com"
-        self._api__key = os.environ['API_KEY_KRAKEN']
-        self._api__secret = os.environ['API_KEY_KRAKEN']
+        self._api__key = os.getenv('API_KEY_KRAKEN')
+        self._api__secret = os.getenv('API_SECRET_KRAKEN')
 
-    def get_kraken_signature(self, urlpath, data):
+    def get_kraken_signature(self, urlpath, data) -> bytes:
 
         postdata = urllib.parse.urlencode(data)
-        encoded = (str(data['none']) + postdata).encode()
+        encoded = (str(data['nonce']) + postdata).encode()
         message = urlpath.encode() + hashlib.sha256(encoded).digest()
 
-        mac = hmac.new(base64.b64decode(self._api_secret), message, hashlib.sha512)
+        mac = hmac.new(base64.b64decode(self._api__secret), message, hashlib.sha512)
         sigdigest = base64.b64encode(mac.digest())
         return sigdigest.decode()
 
     # Attaches auth headers and returns results of a POST request
-    def kraken_request(self, uri_path, data, api_key):
+    def kraken_request(self, uri_path, data):
         headers = {}
-        headers['API-Key'] = api_key
+        headers['API-Key'] = self._api__key
         # get_kraken_signature() as defined in the 'Authentication' section
-        headers['API-Sign'] = get_kraken_signature(self, uri_path, data)             
+        headers['API-Sign'] = self.get_kraken_signature(uri_path, data)             
         req = requests.post((self._api__url + uri_path), headers=headers, data=data)
         return req
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def dca():
-    print('hellooooo')
+import os
+import time
+import requests
+from classes.kraken import KrakenClient
+
+def getAccountBalance():
+    # Construct the request and print the result
+    resp = KrakenClient().kraken_request('/0/private/Balance', {
+        "nonce": str(int(1000*time.time()))
+    })
+    return resp.json()
 
 
 if __name__ == "__main__":
-    dca()
+    print(getAccountBalance())
